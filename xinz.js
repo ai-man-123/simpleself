79b9d682e7b9ae9c26775d11f4cfa6e343b4a153
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-10-11 15:29:26 +0800

    Update xinz.js

--- xinz.js
+++ xinz.js
@@ -34,7 +34,7 @@ const exif = new Exif();
 conn.connect()
 const xinz = conn.xinz
 
-fake = "© 𝐗𝐢𝐧𝐳-𝐓𝐞𝐚𝐦"
+fake = "© ✪፝͜͡✮⃝ꋬ꒐ꂵꋬꋊメ₄ᷧ⃝͜͡𓃮"
 fakeimage = fs.readFileSync("./media/aqul.jpeg")
 prefix = 'z'
 blocked = []
@@ -128,7 +128,7 @@ xinz.on('message-new', async(qul) => {
         if (isCmd && isGroup) {console.log(color('[CMD]'), color(moment(qul.messageTimestamp * 1000).format('DD/MM/YY HH:mm:ss'), 'yellow'), color(`${command} [${args.length}]`), 'from', color(xinz.user.name), 'in', color(groupName))}
         switch (command) {
 			case 'menu': case 'help':
-				textnya = `	𝐗𝐈𝐍𝐙-𝐒𝐄𝐋𝐅𝐁𝐎𝐓
+				textnya = `	𝐒𝐄𝐋𝐅𝐁𝐎𝐓
 
 \`\`\`▢ ${prefix}sticker\`\`\`
 \`\`\`▢ ${prefix}swm nama | author\`\`\`
@@ -175,7 +175,7 @@ xinz.on('message-new', async(qul) => {
 \`\`\`▢ ${prefix}antidelete\`\`\`
 \`\`\`▢ ${prefix}tourl <media>\`\`\`
 
-*© Xinz-Team*`
+*© Aiman-Team*`
 				aqul.sendFakeStatusWithImg(from, fakeimage, textnya, fake)
 				break
             case 'test':
